6a137b1d1dbf3995d31e93ea5ce3784fd2a5da03
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-03-12 20:15:52 +0100

    Initial Folder setup complete

--- EMS/ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
+++ EMS/ServerLibrary/obj/Debug/net8.0/ServerLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a5b6e7ab97b6e8ae8822246ae47bd623dc0eaad")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
