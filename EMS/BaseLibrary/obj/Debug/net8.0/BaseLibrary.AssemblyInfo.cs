020e47ad89038d63becb8e99759659fdc0b6ebd1
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-03-12 21:05:14 +0100

    Creating initial models for Base Library

--- EMS/BaseLibrary/obj/Debug/net8.0/BaseLibrary.AssemblyInfo.cs
+++ EMS/BaseLibrary/obj/Debug/net8.0/BaseLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaseLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a5b6e7ab97b6e8ae8822246ae47bd623dc0eaad")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a137b1d1dbf3995d31e93ea5ce3784fd2a5da03")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaseLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaseLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
