5a0bd4c4d5450fdc32d59fce6198c2b523c688ae
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-03-15 12:00:06 +0100

    seperating login and register pages

--- EMS/Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
+++ EMS/Client/obj/Debug/net8.0/Client.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Client")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9bf998aae5d8eeda9b3e28bbe60e522e1871076")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17ba8616b65c4aabe46c36a5f6fa19e2300261df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Client")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Client")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
