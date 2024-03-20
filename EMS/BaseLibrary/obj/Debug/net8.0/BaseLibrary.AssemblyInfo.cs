4e4e9f0dfe063e270d44337d2feca2824268b98c
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-03-20 16:50:00 +0100

    adding the top menu

--- EMS/BaseLibrary/obj/Debug/net8.0/BaseLibrary.AssemblyInfo.cs
+++ EMS/BaseLibrary/obj/Debug/net8.0/BaseLibrary.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BaseLibrary")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d4595a80b123ca996a47fdcd9e4a15d177bbdba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3be10055be2d1c4beb98604482d7609a93d9485")]
 [assembly: System.Reflection.AssemblyProductAttribute("BaseLibrary")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BaseLibrary")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
