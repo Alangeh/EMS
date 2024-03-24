7b62e51c2a17e0bf0762a9ca76ab21bbe014d51e
Lionel Ngobesing <ngobesinglionel@gmail.com>
2024-03-24 21:55:46 +0100

    add to DBset new models

--- EMS/ServerLibrary/Data/AppDbContext.cs
+++ EMS/ServerLibrary/Data/AppDbContext.cs
@@ -10,11 +10,25 @@ namespace ServerLibrary.Data
         public DbSet<GeneralDepartment> GeneralDepartments { get; set; }
         public DbSet<Department> Departments { get; set; }
         public DbSet<Branch> Branches { get; set; }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<City> Cities { get; set; } 
         public DbSet<Town> Towns { get; set; }
+
+        // Authentication Tables
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
         public DbSet<SystemRole> SystemRoles { get; set; }
         public DbSet<UserRole> UserRoles { get; set; }
         public DbSet<RefreshTokenInfo> RefreshTokenInfos { get; set; }
+
+        // Others
+        public DbSet<Vacation> Vacations { get; set; }
+        public DbSet<VacationType> VacationTypes { get; set; }
+        public DbSet<Sanction> Sanctions { get; set; }
+        public DbSet<SanctionType> sanctionTypes { get; set; }
+        public DbSet<OverTime> OverTimes { get; set; }
+        public DbSet<OverTimeType> OverTimesTypes { get; set; }
+        public DbSet<Health> Healths { get; set; }
         #endregion
     }
 }
